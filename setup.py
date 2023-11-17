f5e0ed4d1ac0edbf51ec46cc659d20506f7b94a2
Chris Agostino <cagostino@hubspot.com>
2023-11-17 14:33:28 -0500

    updated a typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chroptiks',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'matplotlib', 'numpy', 'scipy'  # Add your dependencies here
@@ -10,7 +10,10 @@ setup(
     # Additional metadata
     author='Chris Agostino',
     author_email='cjp.agostino@gmail.com',
-    description='A set of helper functions for quickly making matplotlib plots using latex fonts andwith a high number of built-in options.',
+    long_description=open('README.md').read(),  # or README.rst if using RST
+    long_description_content_type='text/markdown',
+
+    description='A set of helper functions for quickly making matplotlib plots using latex fonts and with a high number of built-in options.',
     license='MIT',
     keywords='matplotlib plotting utilities',
     url='https://github.com/cagostino/chroptiks', # Optional
