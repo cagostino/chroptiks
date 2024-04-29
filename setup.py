c7e34a0c97492da530e688a55f5224becc56b334
cagostino <cjp.agostino@gmail.com>
2024-04-29 02:11:41 -0400

    had to adjust readme to rst for the pypi publishing

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chroptiks',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     install_requires=[
         'matplotlib', 'numpy', 'scipy'  # Add your dependencies here
@@ -10,8 +10,8 @@ setup(
     # Additional metadata
     author='Chris Agostino',
     author_email='cjp.agostino@gmail.com',
-    long_description=open('README.md').read(),  # or README.rst if using RST
-    long_description_content_type='text/markdown',
+    long_description=open('README.rst').read(),  # or README.rst if using RST
+    long_description_content_type='text/x-rst',  # This is important for rendering the README on PyPI
 
     description='A set of helper functions for quickly making matplotlib plots with a high number of built-in options.',
     license='MIT',
